ca3b94f45fafc3d1dac4e7eaa7d8b9520dcc2292
EricRLucas <eric.lucas@lstmed.ac.uk>
2023-06-16 14:49:01 +0100

    repeated phenotype randomisations with samples removed

--- randomisations/create_random_permutations.r
+++ randomisations/create_random_permutations.r
@@ -2,19 +2,32 @@ library(data.table)
 library(stringr)
 
 arg.values <- commandArgs(trailingOnly=T)
-cat('Running with metadata:', arg.values[1], '\n', sep = '\n')
 
-meta_fn <- arg.values[1]
-output_fn <- arg.values[2]
+meta.fn <- arg.values[1]
+output.fn <- arg.values[2]
+{if (length(arg.values) == 2)
+	samples.to.remove <- 'none'
+else
+	samples.to.remove <- readLines(arg.values[3])
+}
+
+arg.values <- commandArgs(trailingOnly=T)
+cat('Running with metadata:', meta.fn, 
+    'and output filename:', output.fn, 
+    'samples to remove:', paste(samples.to.remove, sep = ','), '\n', sep = '\n')
+
 # Load the phenotypes (original GAARD metadata) and the VObs metadata (provided by Sanger, contains only 
 # sequenced samples. 
 phenotypes <- fread('../data/sample_phenotypes_EA.csv', key = 'specimen')
-vobs.meta <- fread(meta_fn, key = 'partner_sample_id')
-# Remove males
-vobs.meta <- vobs.meta[sex_call == 'F', ]
+vobs.meta <- fread(meta.fn, key = 'partner_sample_id')
+# Remove samples that need excluding
+samples.to.include <- setdiff(
+	vobs.meta[sex_call == 'F', partner_sample_id],
+	samples.to.remove
+)
 
 # Reduced the phenotype table to only include sequenced samples
-phenotypes <- phenotypes[vobs.meta$partner_sample_id, ]
+phenotypes <- phenotypes[samples.to.include]
 
 # Have a column to indicate population (location x species x insecticide)
 phenotypes$population <- with(phenotypes, paste(location, species, insecticide, sep = '.'))
@@ -35,5 +48,5 @@ replicate.names <- paste('r', str_pad(1:num.randomisations, nchar(as.character(n
 phenotypes[, (replicate.names) := shuffle(phenotype, num.randomisations), by = population]
 
 # Write the table to file
-fwrite(phenotypes, output_fn, sep = '\t', quote = F)
+fwrite(phenotypes, output.fn, sep = '\t', quote = F)
 
