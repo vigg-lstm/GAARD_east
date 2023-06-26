ba936a513f322740690dced7a73223a5ee8b9388
EricRLucas <eric.lucas@lstmed.ac.uk>
2023-06-26 21:14:00 +0100

    Ran focal gwas

---
+++ focal_gwas/Fst/focal_gwas_Fst_tanzania.sh
@@ -0,0 +1,4 @@
+Rscript focal_gwas_Fst_tanzania.r Moshi_arabiensis_Delta > Moshi_arabiensis_Delta_focal_gwas.log 2>&1 &
+Rscript focal_gwas_Fst_tanzania.r Moshi_arabiensis_PM > Moshi_arabiensis_PM_focal_gwas.log 2>&1
+Rscript focal_gwas_Fst_tanzania.r Muleba_arabiensis_Delta > Muleba_arabiensis_Delta_focal_gwas.log 2>&1
+
---
+++ focal_gwas/H12/focal_gwas_H12_tanzania.sh
@@ -0,0 +1,3 @@
+Rscript focal_gwas_H12_tanzania.r Moshi_arabiensis_Delta > Moshi_arabiensis_Delta_focal_gwas.log 2>&1 &
+Rscript focal_gwas_H12_tanzania.r Moshi_arabiensis_PM Ace1 > Moshi_arabiensis_PM_focal_gwas.log 2>&1
+Rscript focal_gwas_H12_tanzania.r Muleba_arabiensis_Delta > Muleba_arabiensis_Delta_focal_gwas.log 2>&1
