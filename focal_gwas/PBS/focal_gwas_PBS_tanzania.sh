7451de32ccd78e0c3120cf4b2d83feabcbf28eae
EricRLucas <eric.lucas@lstmed.ac.uk>
2023-09-11 11:25:23 +0100

    Reran focal_gwas and haplotyp association analysis after having rerun randomisations

--- focal_gwas/PBS/focal_gwas_PBS_tanzania.sh
+++ focal_gwas/PBS/focal_gwas_PBS_tanzania.sh
@@ -1,3 +1,3 @@
 Rscript focal_gwas_PBS_tanzania.r Moshi_arabiensis_Delta > Moshi_arabiensis_Delta_focal_gwas.log 2>&1 &
-Rscript focal_gwas_PBS_tanzania.r Moshi_arabiensis_PM > Moshi_arabiensis_PM_focal_gwas.log 2>&1
+Rscript focal_gwas_PBS_tanzania.r Moshi_arabiensis_PM > Moshi_arabiensis_PM_focal_gwas.log 2>&1 &
 Rscript focal_gwas_PBS_tanzania.r Muleba_arabiensis_Delta > Muleba_arabiensis_Delta_focal_gwas.log 2>&1
